96cb7a4f35c770e28f22ef8fd9446c46ebf04fe8
TIANRUI FAN <tianruifan21@gmail.com>
2025-12-25 00:11:06 +0800

    add CORS configure

--- Word_Image_Extractor/frontend/next-env.d.ts
+++ Word_Image_Extractor/frontend/next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-/// <reference types="next/navigation-types/compat/navigation" />
 
-// 注意：此文件由Next.js自动维护。
-
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
